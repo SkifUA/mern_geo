25f251b1e5dfcb6da6b8b563b094f466e65e9623
Valerii Kuzin <vkuzin@zaraffasoft.com>
2020-04-25 17:25:59 +0300

    #13 Added a Modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react'
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 import './PlaceItem.css'
 
 class PlaceItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showMap: false
+    }
+  }
+
+  openMapHandler = () => {
+    this.setState({showMap: true})
+  }
+
+  closeMapHandler = () => {
+    this.setState({showMap: false})
+  }
 
   render() {
     const props = this.props
 
-    return <li className="place-item">
+    return <React.Fragment>
+      <Modal
+        show={this.state.showMap}
+        onCancel={this.closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-action"
+        footer={<Button onClick={this.closeMapHandler}>Close</Button>}
+      >
+        <div className="map-container">
+          <h2>The Map</h2>
+        </div>
+      </Modal>
+      <li className="place-item">
       <Card className="place-item__content">
         <div className="place-item__image">
           <img src={props.image} alt={props.title} />
@@ -20,12 +48,13 @@ class PlaceItem extends Component {
           <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-          <Button inverse>View On Map</Button>
+          <Button inverse onClick={this.openMapHandler}>View On Map</Button>
           <Button to={`/places/${props.id}`}>Edit</Button>
           <Button danger>Delete</Button>
         </div>
       </Card>
     </li>
+    </React.Fragment>
   }
 }
 
