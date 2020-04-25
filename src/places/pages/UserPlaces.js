735a857c8bca2c269be2975a0a9d9fee08ae1f9a
Valerii Kuzin <vkuzin@zaraffasoft.com>
2020-04-25 14:41:58 +0300

    #11 Added Getting Route Params for function Component

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom'
 
 import PlaceList from "../components/PlaceList";
 
-class UserPlaces extends Component {
-  constructor() {
-    super();
-
-    this.DUMMY_PLACES = [
-      {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers of the world',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St, New York, NY',
-        location: {
-          lan: 40.7484405,
-          lng: -73.9878584
-        },
-        creator: 'u1'
-      },
-      {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers of the world',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St, New York, NY',
-        location: {
-          lan: 40.7484405,
-          lng: -73.9878584
-        },
-        creator: 'u2'
-      }
-    ]
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers of the world',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+    address: '20 W 34th St, New York, NY',
+    location: {
+      lan: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers of the world',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+    address: '20 W 34th St, New York, NY',
+    location: {
+      lan: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
   }
+];
 
-  render() {
-    return <PlaceList items={this.DUMMY_PLACES} />
-  }
+// TODO make via class !!!
+function UserPlaces() {
+  const userId = useParams().userId
+  const loadedPlace = DUMMY_PLACES.filter(place => place.creator === userId)
+
+  return <PlaceList items={loadedPlace} />
 }
 
 export default UserPlaces
