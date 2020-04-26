48688fce3620cf291d29021e7829bad2dcb3ca29
Valerii Kuzin <vkuzin@zaraffasoft.com>
2020-04-26 21:52:01 +0300

    #24 Added Auth Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Users from "./user/pages/Users";
 import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth"
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
@@ -24,6 +25,9 @@ const App = () => {
         </Route>
         <Route path="/places/:placeId" exact>
           <UpdatePlace />
+        </Route>
+        <Route path="/auth" exact>
+          <Auth />
         </Route>
         <Redirect to="/" />
       </Switch>
