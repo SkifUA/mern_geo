541876fc6ced7bc8b744b12fe71716ad76b9194c
Valerii Kuzin <vkuzin@zaraffasoft.com>
2020-04-26 16:58:24 +0300

    #20 Cleaned

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from "./user/pages/Users";
 import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
+import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
@@ -12,17 +13,20 @@ const App = () => {
     <MainNavigation />
     <main>
       <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Route path="/:userId/places" exact>
-        <UserPlaces />
-      </Route>
-      <Route path="/places/new" exact>
-        <NewPlace />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId" exact>
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </main>
   </Router>
   )
